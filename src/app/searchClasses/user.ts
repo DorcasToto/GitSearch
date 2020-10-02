de63742ef79479eebd484c36717e2f6badfcf065
Moringa School <dorcasmosonik@gmail.com>
2020-10-02 15:58:12 +0300

    bootstrap

--- src/app/searchClasses/user.ts
+++ src/app/searchClasses/user.ts
@@ -1,2 +1,13 @@
 export class User {
+    constructor(public username: string,
+        public image: string,
+        public followers: number,
+        public following: number,
+        public bio: string,
+        public location: string,
+        public emailaddress: string,
+        public repos: string
+    ) {
+
+    }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 export const environment = {
   production: false,
   API_KEY :'21db888b386c5118ed297ee407d29cdd7abd7008',
-  API_URL:''
+  API_URL:'https://api.github.com/users/'
 };
